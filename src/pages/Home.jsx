92bf246162cdd2a75564402100819b2f1507302c
Gaurav Vijay JAdhav <gauravvjadhav143@gmail.com>
2023-06-27 13:44:02 +0530

    added react-router-dom to the App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      {' '}
+      <Link to={'/starred'}> Go to Starred Page</Link>
+    </div>
+  );
+};
+
+export default Home;
