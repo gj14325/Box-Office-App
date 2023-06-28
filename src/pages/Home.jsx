a1ef9a2e0e95f59587eafb85fec605039995fc01
Gaurav Vijay JAdhav <gauravvjadhav143@gmail.com>
2023-06-28 22:01:20 +0530

    Added input for search, search for shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState('');
+
+  // console.log(inputValue);
+
+  const onSearchInputChange = event => {
+    // console.log(ev.target.value);
+
+    setSearchStr(event.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
